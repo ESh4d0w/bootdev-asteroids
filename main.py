d2c8c2d99295ec00b96e6c3d95241a5b45982b6d
esh4d0w <jan.reinhard@pm.me>
2025-02-16 17:22:35 +0100

    ticks

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from constants import *
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
+        dt = pygame.tick() / 1000
 
 
 if __name__ == "__main__":
